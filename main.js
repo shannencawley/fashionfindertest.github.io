86c9bbebc8b0ca22353e259ff76d4aa23d3c5466
Eve <95377553+fishyEve@users.noreply.github.com>
2024-03-28 17:46:52 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,19 +1,16 @@
-import { MY_API_KEY } from './config.js';
-
-fetch("           ", {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "quotes15.p.rapidapi.com",
-		"x-rapidapi-key": MY_API_KEY
+const url = 'https://asos-com1.p.rapidapi.com/products/detail?url=only-sons%2Fonly-sons-oversized-twill-worker-jacket-in-camo%2Fprd%2F203998871';
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '7fd8b0ff8emsh61db2357987c018p10e112jsna22e3c2375c3',
+		'X-RapidAPI-Host': 'asos-com1.p.rapidapi.com'
 	}
-})
-.then(response => response.json())
-.then(response => {
-    console.log(response);
-    
-    document.getElementById('quote').innerHTML = response.content;
-    document.getElementById('author').innerHTML = '- ' + response.originator.name + ' -';
-})
-.catch(err => {
-	console.log(err);
-});
+};
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}
