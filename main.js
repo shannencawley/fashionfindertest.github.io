edf1fae6c516cfbfb016af0b54627f644c822289
shannencawley <63126866+shannencawley@users.noreply.github.com>
2024-03-28 19:25:27 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,19 +1,16 @@
-const url = 'https://asos-marketplace.p.rapidapi.com/product?productID=133831038';
+const url = 'https://kohls.p.rapidapi.com/products/list?limit=24&offset=1&dimensionValueID=AgeAppropriate%3ATeens';
 const options = {
 	method: 'GET',
 	headers: {
 		'X-RapidAPI-Key': '3c5f513caamsha93acf9bea18ed0p15a0aejsnccbf9aa5430b',
-		'X-RapidAPI-Host': 'asos-marketplace.p.rapidapi.com'
+		'X-RapidAPI-Host': 'kohls.p.rapidapi.com'
 	}
 };
 
 try {
-	
 	const response = await fetch(url, options);
 	const result = await response.text();
 	console.log(result);
-	
 } catch (error) {
 	console.error(error);
 }
-;
