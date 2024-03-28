51c57d3e3775eb11db6a7f8e5d9d47fdea61ae98
shannencawley <63126866+shannencawley@users.noreply.github.com>
2024-03-28 18:09:37 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-const url = 'https://asos-com1.p.rapidapi.com/products/detail?url=only-sons%2Fonly-sons-oversized-twill-worker-jacket-in-camo%2Fprd%2F203998871';
+const url = 'https://asos-marketplace.p.rapidapi.com/product?productID=133831038';
 const options = {
 	method: 'GET',
 	headers: {
-		'X-RapidAPI-Key': '7fd8b0ff8emsh61db2357987c018p10e112jsna22e3c2375c3',
-		'X-RapidAPI-Host': 'asos-com1.p.rapidapi.com'
+		'X-RapidAPI-Key': '3c5f513caamsha93acf9bea18ed0p15a0aejsnccbf9aa5430b',
+		'X-RapidAPI-Host': 'asos-marketplace.p.rapidapi.com'
 	}
 };
 
