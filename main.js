9c7ffce3192b6889dcbff45dacd705694323f8ec
shannencawley <63126866+shannencawley@users.noreply.github.com>
2024-03-28 19:12:31 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-/*const url = 'https://asos-marketplace.p.rapidapi.com/product?productID=133831038';
+const url = 'https://asos-marketplace.p.rapidapi.com/product?productID=133831038';
 const options = {
 	method: 'GET',
 	headers: {
@@ -16,24 +16,4 @@ try {
 } catch (error) {
 	console.error(error);
 }
-*/
-const url = 'https://asos-marketplace.p.rapidapi.com/product?productID=133831038';
-const options = {
-    method: 'GET',
-    headers: {
-        'X-RapidAPI-Key': 'YOUR_API_KEY', // Make sure to replace YOUR_API_KEY with your actual API key
-        'X-RapidAPI-Host': 'asos-marketplace.p.rapidapi.com'
-    }
-};
-
-async function fetchProduct() {
-    try {
-        const response = await fetch(url, options);
-        const result = await response.text(); // Or response.json() if the response is in JSON format
-        console.log(result);
-    } catch (error) {
-        console.error('Fetch error:', error);
-    }
-}
-
-fetchProduct();
+;
