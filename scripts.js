1de82661321252b14229aa942b1ecc40b4c7948b
shannencawley <63126866+shannencawley@users.noreply.github.com>
2024-04-12 10:39:25 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,14 +1,1 @@
-<script>
-function fetchSearchResults(query) {
-  var apiKey = 'AIzaSyAK2z9rZdXSLlJ9piz24KR1GIpg2t_O08g';
-  var engineId = 'e42db96d918aa4f45';
-  var url = `https://www.googleapis.com/customsearch/v1?q=${encodeURIComponent(query)}&cx=${engineId}&key=${apiKey}`;
 
-  fetch(url)
-    .then(response => response.json())
-    .then(data => {
-      console.log(data); // Process and display your search results here
-    })
-    .catch(error => console.error('Error fetching search results:', error));
-}
-</script>
