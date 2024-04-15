3df7970aa8f8c520889bbe4b95415f4ce5e8b690
shannencawley <63126866+shannencawley@users.noreply.github.com>
2024-04-15 10:41:26 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,17 +1,4 @@
 // scripts.js
-function formatElementInfo(element) {
-    let title = element.getAttribute('title'); // Get the title attribute
-    let src = element.getAttribute('src'); // Get the src attribute
-
-    const info = {
-        tagName: element.tagName,
-        classList: Array.from(element.classList),
-        src: src, // This will be the src attribute of the image
-        title: title // This will be the title attribute of the image
-    };
-
-    return JSON.stringify(info, null, 2);
-}
 
 // Helper function to format the information of an element
 function formatElementInfo(element) {
