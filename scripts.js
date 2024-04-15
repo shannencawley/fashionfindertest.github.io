54cda1035f3215d48b58831d371f0bfb9e4e7aa4
shannencawley <63126866+shannencawley@users.noreply.github.com>
2024-04-15 10:26:31 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,51 +1,29 @@
 // scripts.js
 function formatElementInfo(element) {
-    let content;
-    switch (element.tagName) {
-        case 'INPUT':
-            content = element.value; // Get value from input elements
-            break;
-        case 'IMG':
-            content = element.src; // Get src from img elements
-            break;
-        default:
-            content = element.innerHTML.trim(); // Use innerHTML for other elements
-            break;
-    }
-    
+    let title = element.getAttribute('title'); // Get the title attribute
+    let src = element.getAttribute('src'); // Get the src attribute
+
     const info = {
         tagName: element.tagName,
         classList: Array.from(element.classList),
-        content: content // This will be the value or src or innerHTML depending on element type
+        src: src, // This will be the src attribute of the image
+        title: title // This will be the title attribute of the image
     };
-    
+
     return JSON.stringify(info, null, 2);
 }
 
-
-// Event listener for when the document is fully loaded
 document.addEventListener('DOMContentLoaded', (event) => {
-    // Find the container in the document where you want to append the new elements
-    const formattedContentDiv = document.getElementById('formatted-content');
-
-    // Event delegation for handling clicks on dynamic 'gs' class elements
+    // Event delegation for handling clicks on dynamic 'gs-image' class elements
     document.addEventListener('click', function(event) {
-        // Check if the clicked element has a class that starts with 'gs'
         let target = event.target;
-        while (target != null) {
-            if (target.matches('[class^="gs"], [class*=" gs"]')) {
-                const formattedOutput = formatElementInfo(target);
-                console.log(formattedOutput);
 
-                // If you want to do something more, like displaying the data on the page,
-                // you could append the formattedOutput to a specific element on the page.
-                // Example: document.getElementById('output').innerHTML = formattedOutput;
-                
-                // Since we found our element, we can break the loop
-                break;
-            }
-            // If we haven't found our element, move up the DOM tree
-            target = target.parentElement;
+        // Check if the clicked element is an IMG and has the 'gs-image' class
+        if (target.tagName === 'IMG' && target.classList.contains('gs-image')) {
+            const formattedOutput = formatElementInfo(target);
+            console.log(formattedOutput);
+            // Optionally, display the formatted information on the page
+            // document.getElementById('output').innerHTML = formattedOutput;
         }
     });
 });
