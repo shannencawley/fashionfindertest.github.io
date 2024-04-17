3d931d395e4f4d4366e4093af82b6220c68a28a5
Eve <95377553+fishyEve@users.noreply.github.com>
2024-04-17 19:53:50 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -16,10 +16,10 @@ function formatElementInfo(element) {
 }
 // trying something here. Code taken from codemyui.com
 jQuery(document).ready(function(){
-   $('h1').mousemove(function(e){
+   $('body').mousemove(function(e){
      var rXP = (e.pageX - this.offsetLeft-$(this).width()/2);
      var rYP = (e.pageY - this.offsetTop-$(this).height()/2);
-     $('h1').css('text-shadow', +rYP/10+'px '+rXP/80+'px rgba(227,6,19,.8), '+rYP/8+'px '+rXP/60+'px rgba(255,237,0,1), '+rXP/70+'px '+rYP/12+'px rgba(0,159,227,.7)');
+     $('body').css('text-shadow', +rYP/10+'px '+rXP/80+'px rgba(227,6,19,.8), '+rYP/8+'px '+rXP/60+'px rgba(255,237,0,1), '+rXP/70+'px '+rYP/12+'px rgba(0,159,227,.7)');
    });
 });
 // END of trying something here. Delete later if this fucks shit up.
