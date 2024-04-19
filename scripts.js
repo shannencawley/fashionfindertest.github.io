f01ddc05cd2349133fabcb3779c8a249737bea32
shannencawley <63126866+shannencawley@users.noreply.github.com>
2024-04-18 20:45:39 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -4,6 +4,67 @@
 // function formatElementINfo creates an object containing these pieces of information every time
 // an image is clicked on our website which converts it to a JSON string and logs it to the console.
 
+
+
+
+
+document.addEventListener('click', function(event) {
+    let target = event.target;
+
+    // Check if the clicked element is an IMG with the class 'gs-image'
+    if (target.tagName === 'IMG' && target.classList.contains('gs-image')) {
+        // Find the ancestor element that is a link, which might contain the URL
+        let linkElement = target.closest('a.gs-previewLink');
+        let imageUrl = linkElement ? linkElement.href : 'URL not found';
+
+        // Functionality to display the image information
+        const imageSrc = target.src; // Gets the source of the image
+        const imageTitle = target.getAttribute('alt'); // Gets the alt text as the title
+
+        // Update the formatElementInfo function to include the URL
+        const formattedOutput = formatElementInfo(target, imageUrl); // Pass the URL to the function
+        console.log(formattedOutput);
+
+        // Create the content you want to display
+        const displayContent = `
+            <div class="image-display">
+                <img src="${imageSrc}" alt="${imageTitle}">
+                <p>${imageTitle}</p>
+                <p>URL: ${imageUrl}</p>
+            </div>
+        `;
+        
+        // Insert the content into the image-info div
+        document.getElementById('image-info').innerHTML = displayContent;
+    }
+});
+
+// Update the formatElementInfo function to accept the URL as an argument
+function formatElementInfo(element, url) {
+    let title = element.getAttribute('title'); // Get the title attribute
+    let src = element.getAttribute('src'); // Get the src attribute
+
+    const info = {
+        tagName: element.tagName,
+        classList: Array.from(element.classList),
+        src: src, // This will be the src attribute of the image
+        title: title, // This will be the title attribute of the image
+        url: url // This will be the URL of the website
+    };
+
+    return JSON.stringify(info, null, 2);
+}
+
+
+
+
+
+
+
+
+
+
+/*
 // Helper function to format the information of an element
 function formatElementInfo(element) {
     let title = element.getAttribute('title'); // Get the title attribute
@@ -44,3 +105,4 @@ document.addEventListener('click', function(event) {
     }
 });
 
+*/
